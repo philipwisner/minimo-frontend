85583e10b9b2c44a41c349567bed86fd1c82cba4
philipwisner <philipwisner@gmail.com>
2017-10-03 17:39:43 +0200

    created models & pages

---
+++ Frontend/Minimo/src/app/models/blog.ts
@@ -0,0 +1,12 @@
+export class Blog {
+  user_id: string;
+  blog_name: string;
+  blog_description: string;
+  blog_image: string;
+  date: Date;
+  // color_theme: string;
+
+  constructor() {
+  }
+
+}
