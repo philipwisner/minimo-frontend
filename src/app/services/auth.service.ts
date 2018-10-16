b902fc96b7141d0ce98fd266f15e11c7a64b2cee
philipwisner <philipwisner@gmail.com>
2018-10-15 22:18:56 -0400

    added google service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,6 @@ const apiUrl = environment.apiUrl + '/auth';
 
 @Injectable()
 export class AuthService {
-
   private initialized: boolean;
   private user: User;
   private userChange: Subject<User | null> = new Subject();
@@ -27,60 +26,70 @@ export class AuthService {
     this.userChange.next(user);
   }
 
-// GET LOGGED IN USER
-  getUser() : User {
+  // GET LOGGED IN USER
+  getUser(): User {
     return this.user;
   }
 
   signup(user: User) {
     let options = new RequestOptions();
     options.withCredentials = true;
-    return this.http.post(apiUrl + '/signup', user, options)
-      .map(res => {
-        let user = new User(res.json());
-        this.setUser(user);
-        return user;
-      });
+    return this.http.post(apiUrl + "/signup", user, options).map(res => {
+      let user = new User(res.json());
+      this.setUser(user);
+      return user;
+    });
   }
 
   login(user: User) {
     let options = new RequestOptions();
     options.withCredentials = true;
-    return this.http.post(apiUrl + '/login', user, options)
-      .map(res => {
-        let user = new User(res.json());
-        this.setUser(user);
-        return user;
-      });
+    return this.http.post(apiUrl + "/login", user, options).map(res => {
+      let user = new User(res.json());
+      this.setUser(user);
+      return user;
+    });
+  }
+
+  google() {
+    console.log('running auth/google function');
+    let options = new RequestOptions();
+    options.withCredentials = true;
+    return this.http.get(apiUrl + "/google", options).map(res => {
+      let user = new User(res.json());
+      this.setUser(user);
+      return user;
+    });
   }
 
   logout() {
     let options = new RequestOptions();
     options.withCredentials = true;
-    return this.http.post(apiUrl + '/logout', {}, options)
-    .map(res => {
+    return this.http.post(apiUrl + "/logout", {}, options).map(res => {
       this.setUser();
       return null;
     });
   }
 
-//CHECK IF LOGGED IN
+  //CHECK IF LOGGED IN
   me() {
     let options = new RequestOptions();
     options.withCredentials = true;
-    return this.http.get(apiUrl + '/me', options)
-      .map(res => {
+    return this.http.get(apiUrl + "/me", options).map(
+      res => {
         let user = new User(res.json());
         this.setUser(user);
         return user;
-      }, (err) => {
-        if(err.status=== 404) {
+      },
+      err => {
+        if (err.status === 404) {
           this.setUser();
         }
-      });
+      }
+    );
   }
 
-//INITIALIZE SESSION
+  //INITIALIZE SESSION
   initialize() {
     if (!this.initialized) {
       this.initialized = true;
@@ -88,16 +97,14 @@ export class AuthService {
     }
   }
 
-
-//EDIT USER INFO (PIC & DESCRIPTION)
-  updateUser(user: User){
+  //EDIT USER INFO (PIC & DESCRIPTION)
+  updateUser(user: User) {
     let options = new RequestOptions();
     options.withCredentials = true;
-    return this.http.put(apiUrl + '/me', user, options)
-      .map((res: Response) => {
-        let user = new User(res.json());
-        this.setUser(user);
-        return user;
-    })
+    return this.http.put(apiUrl + "/me", user, options).map((res: Response) => {
+      let user = new User(res.json());
+      this.setUser(user);
+      return user;
+    });
   }
 }
